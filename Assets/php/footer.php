88df5892d3aaa3f84c8cc26bfc5b6e802de39dbb
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-08-28 11:35:06 +0500

    include and require used to load files(task completed)

---
+++ Assets/php/footer.php
@@ -0,0 +1,123 @@
+<footer class="footer">
+      <section class="footer_section_content">
+        <div class="main_content">
+          <div class="text_content_div">
+            <div class="text_sub_content">
+              <h4 class="section_sub_heading">Company</h4>
+              <ul class="company_list">
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">About</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Approach</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Sustainability</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Careers</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">News</a>
+                </li>
+              </ul>
+            </div>
+            <div class="text_sub_content">
+              <h4 class="section_sub_heading">Company</h4>
+              <ul class="company_list">
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">About</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Approach</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Sustainability</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Careers</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">News</a>
+                </li>
+              </ul>
+            </div>
+            <div class="text_sub_content">
+              <h4 class="section_sub_heading">Company</h4>
+              <ul class="company_list">
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">About</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Approach</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Sustainability</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Careers</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">News</a>
+                </li>
+              </ul>
+            </div>
+            <div class="text_sub_content">
+              <h4 class="section_sub_heading">Company</h4>
+              <ul class="company_list">
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">About</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Approach</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Sustainability</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">Careers</a>
+                </li>
+                <li class="company_list_items">
+                  <a href="/" class="company_item_link">News</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="img_content_div">
+            <div class="company_image">
+              <img src="./Assets/images/Company_img1.png" alt="Company Image" />
+            </div>
+            <div class="company_image">
+              <img src="./Assets/images/Company_img2.png" alt="Company Image" />
+            </div>
+            <div class="company_image">
+              <img src="./Assets/images/Company_img3.png" alt="Company Image" />
+            </div>
+            <div class="company_image">
+              <img src="./Assets/images/Company_img4.png" alt="Company Image" />
+            </div>
+          </div>
+        </div>
+        <div class="sub_content">
+          <div class="text_content_footer_bottom">
+            <ul class="footer_items">
+              <li class="footer_list_item">
+                <span id="copyright">&copy; Themes by psdfreebies.com</span>
+              </li>
+
+              <li class="footer_list_item">
+                <a class="footer_list_item_link" href="/">Privacy</a>
+              </li>
+
+              <li class="footer_list_item">
+                <a class="footer_list_item_link" href="/">Terms of Use</a>
+              </li>
+
+              <li class="footer_list_item">
+                <a class="footer_list_item_link" href="/">Site Map</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </footer>
---
+++ Assets/php/header.php
@@ -0,0 +1,32 @@
+<!-- Header -->
+<div class="landing_Page_container"></div>
+    <header class="header">
+      <nav class="navbar">
+        <div class="logo">
+          <img
+            src="./Assets/images/BlueRex_Logo.png"
+            alt="BlueRex Logo"
+            class="rexlogo"
+          />
+          <span class="rexlogo_text">BlueRex</span>
+        </div>
+        <div class="list_container">
+          <ul class="header_items_list">
+            <li class="list_items">
+              <a href="./index.html" class="li_links">Home</a>
+            </li>
+            <li class="list_items"><a href="/" class="li_links">About</a></li>
+            <li class="list_items">
+              <a href="/" class="li_links">Features</a>
+            </li>
+            <li class="list_items"><a href="/" class="li_links">Product</a></li>
+            <li class="list_items"><a href="/" class="li_links">Reviews</a></li>
+            <li class="list_items"><a href="/" class="li_links">Faq</a></li>
+            <li class="list_items"><a href="/" class="li_links">Contact</a></li>
+          </ul>
+          <button class="btn">
+            <span class="bars"></span>
+          </button>
+        </div>
+      </nav>
+    </header>
